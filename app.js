86f8f5d26e321384201f324393128a9011d5444d
Milo_Lynch <Milobravman20@gmail.com>
2022-07-20 18:46:33 -0400

    setting up post route for responces

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('responses', (req,res)=>{
+    console.log(res);
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })

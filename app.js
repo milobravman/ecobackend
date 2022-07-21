ade47dd41ccc25af485e1b0a6f5a0fe9dd7a7410
Milo_Lynch <Milobravman20@gmail.com>
2022-07-21 12:02:05 -0400

    cors policy unblocked

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next();
+})
+
 app.post('/responses', (req,res)=>{
     console.log(res);
 })

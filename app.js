e09deea5cd0e5826f7f01261d899bb2ab35f73d2
Milo_Lynch <Milobravman20@gmail.com>
2022-07-22 19:39:27 -0400

    finished backend basic setup

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 3001
+const bodyParser = require('body-parser');
+
+
+var jsonParser = bodyParser.json()
+//var urlencodedParser = bodyParser.urlencoded({extended: false})
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+// Parse JSON bodies (as sent by API clients)
+
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  express.json()
+  bodyParser.json()
   next();
 })
 
-app.post('/responses', (req,res)=>{
-    console.log(res);
+app.post('/responses', jsonParser , (req,res)=>{
+    //console.log(res);
+    console.log(req.body.value);
 })
 
 app.listen(port, () => {

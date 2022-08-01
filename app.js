e7ab39035174624cf809afb972bc0a169ce7c469
Milo_Lynch <Milobravman20@gmail.com>
2022-08-01 14:39:50 -0400

    email feature more developed

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.get('/', (req, res) => {
 
 // Parse JSON bodies (as sent by API clients)
 
+var message = ''
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
@@ -27,31 +28,31 @@ app.use((req, res, next) => {
 app.post('/responses', jsonParser , (req,res)=>{
     //console.log(res);
     console.log(req.body.value);
+    message = req.body.value
+    send();
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-var transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'milolynch11@gmail.com',
-    pass: 'ninjadog'
+    user: 'ecodc3@gmail.com',
+    pass: ''
   }
 });
 
-var mailOptions = {
-  from: 'milolynch11@gmail.com',
-  to: 'wendylynch31@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
+
+async function send() {
+    const result = await transporter.sendMail({
+        from: 'ecodc3@gmail.com',
+        to: 'wendylynch31@gmail.com',
+        subject: 'Message from EcoDC',
+        text: message
+    });
+
+    console.log(JSON.stringify(result, null, 4));
+}

90f840cec1c95f16635b51013f01fec6a106261e
Milo_Lynch <Milobravman20@gmail.com>
2022-08-01 12:54:12 -0400

    adding email feature using nodemailer

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3001
 const bodyParser = require('body-parser');
-
+const nodemailer = require('nodemailer');
 
 var jsonParser = bodyParser.json()
 //var urlencodedParser = bodyParser.urlencoded({extended: false})
@@ -32,3 +32,26 @@ app.post('/responses', jsonParser , (req,res)=>{
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'milolynch11@gmail.com',
+    pass: 'ninjadog'
+  }
+});
+
+var mailOptions = {
+  from: 'milolynch11@gmail.com',
+  to: 'wendylynch31@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
